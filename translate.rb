64406080d81db2e909971d3ee40f358b47ae1902
fartem <jaman.smlnsk@gmail.com>
2020-05-09 13:07:19 +0300

    2020-05-09 Update: Added text Language detect

--- translate.rb
+++ translate.rb
@@ -2,12 +2,18 @@
 
 require 'net/http'
 require 'uri'
+require 'whatlanguage'
+
+what_language = WhatLanguage.new(:english, :russian)
+
+input_text = ARGV[0]
+lang = what_language.language(input_text) == :english ? 'en-ru' : 'ru-en'
 
 uri = URI('https://translate.yandex.net/api/v1.5/tr.json/translate')
 query_parameters = {
     :key => '',
-    :text => 'Test',
-    :lang => 'en-ru'
+    :text => input_text,
+    :lang => lang
 }
 uri.query = URI.encode_www_form(query_parameters)
 puts translate_request = Net::HTTP.get(uri)
